407dab1cae3dfbbd07c6082700c742bef3089fd6
A1rdent <100067458+A1rdent@users.noreply.github.com>
2026-09-09 20:09:52 +0300

    Add companion spawner to creative inventory

--- CompanionMod/src/main/java/com/example/companionmod/registry/ItemRegistry.java
+++ CompanionMod/src/main/java/com/example/companionmod/registry/ItemRegistry.java
@@ -2,6 +2,7 @@ package com.example.companionmod.registry;
 
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 
 import com.example.companionmod.common.item.CompanionSpawnerItem;
@@ -11,7 +12,7 @@ public final class ItemRegistry {
     public static final Item COMPANION_SPAWNER = Registry.register(
             Registry.ITEM,
             new ResourceLocation(FabricCompanionMod.MOD_ID, "companion_spawner"),
-            new CompanionSpawnerItem(new Item.Properties().stacksTo(1))
+            new CompanionSpawnerItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC))
     );
 
     private ItemRegistry() {}
