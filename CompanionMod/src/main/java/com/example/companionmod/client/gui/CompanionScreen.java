b45f361809e1e137f7af23e2abbd337764f64a35
A1rdent <100067458+A1rdent@users.noreply.github.com>
2026-09-09 20:12:53 +0300

    Replace client-side GUI toggles with real server button packets and clean companion layout

--- CompanionMod/src/main/java/com/example/companionmod/client/gui/CompanionScreen.java
+++ CompanionMod/src/main/java/com/example/companionmod/client/gui/CompanionScreen.java
@@ -1,35 +1,19 @@
 package com.example.companionmod.client.gui;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.Minecraft;
-
-import com.example.companionmod.fabric.FabricCompanionMod;
-import com.example.companionmod.common.entity.CompanionEntity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
 
 public class CompanionScreen extends AbstractContainerScreen<CompanionScreenHandler> {
-    private static final int TEXTURE_WIDTH = 176;
-    private static final int TEXTURE_HEIGHT = 222;
-    private static final ResourceLocation TEXTURE = new ResourceLocation(FabricCompanionMod.MOD_ID, "textures/gui/companion_gui.png");
+    private static final int GUI_WIDTH = 320;
+    private static final int GUI_HEIGHT = 222;
 
-    private final CompanionEntity companion;
-    private Button mineButton;
-    private Button followButton;
-    private Button stopButton;
-    private Button gatherButton;
-
-    public CompanionScreen(CompanionScreenHandler handler, Inventory playerInventory, Component component) {
-        super(handler, playerInventory, component);
-        this.companion = handler != null ? handler.getCompanion() : null;
-        this.imageWidth = TEXTURE_WIDTH;
-        this.imageHeight = TEXTURE_HEIGHT;
+    public CompanionScreen(CompanionScreenHandler handler, Inventory playerInventory, Component title) {
+        super(handler, playerInventory, title);
+        this.imageWidth = GUI_WIDTH;
+        this.imageHeight = GUI_HEIGHT;
     }
 
     @Override
@@ -38,83 +22,51 @@ public class CompanionScreen extends AbstractContainerScreen<CompanionScreenHand
         this.leftPos = (this.width - this.imageWidth) / 2;
         this.topPos = (this.height - this.imageHeight) / 2;
 
-        // Mine button
-        this.mineButton = this.addRenderableWidget(new Button(
-                this.leftPos + 10, this.topPos + 20, 70, 20,
-                Component.literal("Mine"), (button) -> this.sendCommand("mine")
-        ));
+        int x = this.leftPos + 186;
+        int y = this.topPos + 48;
+        int w = 122;
+        int h = 20;
+        int gap = 25;
 
-        // Follow button
-        this.followButton = this.addRenderableWidget(new Button(
-                this.leftPos + 96, this.topPos + 20, 70, 20,
-                Component.literal("Follow"), (button) -> this.sendCommand("follow")
-        ));
-
-        // Gather button
-        this.gatherButton = this.addRenderableWidget(new Button(
-                this.leftPos + 10, this.topPos + 50, 70, 20,
-                Component.literal("Gather"), (button) -> this.sendCommand("gather")
-        ));
-
-        // Stop button
-        this.stopButton = this.addRenderableWidget(new Button(
-                this.leftPos + 96, this.topPos + 50, 70, 20,
-                Component.literal("Stop"), (button) -> this.sendCommand("stop")
-        ));
+        this.addRenderableWidget(new Button(x, y, w, h, Component.literal("Mine"), b -> press(0)));
+        this.addRenderableWidget(new Button(x, y + gap, w, h, Component.literal("Follow"), b -> press(1)));
+        this.addRenderableWidget(new Button(x, y + gap * 2, w, h, Component.literal("Gather"), b -> press(2)));
+        this.addRenderableWidget(new Button(x, y + gap * 3, w, h, Component.literal("Deposit"), b -> press(3)));
+        this.addRenderableWidget(new Button(x, y + gap * 4, w, h, Component.literal("Stop"), b -> press(4)));
     }
 
-    private void sendCommand(String command) {
-        if (this.companion != null) {
-            switch (command) {
-                case "mine":
-                    this.companion.setMining(!this.companion.isMining());
-                    break;
-                case "follow":
-                    this.companion.setFollowing(!this.companion.isFollowing());
-                    break;
-                case "gather":
-                    this.companion.setGathering(!this.companion.isGathering());
-                    break;
-                case "stop":
-                    this.companion.stopAll();
-                    break;
-            }
+    private void press(int buttonId) {
+        if (this.minecraft != null && this.minecraft.gameMode != null) {
+            this.minecraft.gameMode.handleInventoryButtonClick(this.menu.containerId, buttonId);
         }
     }
 
     @Override
-    public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
-        this.renderBg(pPoseStack);
-        super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
-        this.renderLabels(pPoseStack, pMouseX, pMouseY);
+    protected void renderBg(PoseStack poseStack, float partialTick, int mouseX, int mouseY) {
+        int x = this.leftPos;
+        int y = this.topPos;
+
+        fill(poseStack, x, y, x + GUI_WIDTH, y + GUI_HEIGHT, 0xFF202020);
+        fill(poseStack, x + 4, y + 4, x + 176, y + 100, 0xFF303030);
+        fill(poseStack, x + 4, y + 104, x + 176, y + 218, 0xFF303030);
+        fill(poseStack, x + 180, y + 4, x + 316, y + 218, 0xFF303030);
     }
 
-    protected void renderBg(PoseStack pPoseStack) {
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        this.blit(pPoseStack, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
-    }
+    @Override
+    protected void renderLabels(PoseStack poseStack, int mouseX, int mouseY) {
+        this.font.draw(poseStack, Component.literal("Companion"), 188, 12, 0xFFFFFF);
+        this.font.draw(poseStack, Component.literal("Inventory"), 8, 7, 0xFFFFFF);
+        this.font.draw(poseStack, Component.literal("Your inventory"), 8, 95, 0xFFFFFF);
 
-    private void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
-        pPoseStack.pushPose();
-        pPoseStack.translate(this.leftPos, this.topPos, 0);
-
-        // Render companion info
-        if (this.companion != null) {
-            this.font.draw(pPoseStack, Component.literal("Companion"), 8, 6, 4210752);
-            this.font.draw(pPoseStack, Component.literal("Health: " + (int)this.companion.getHealth() + "/" + (int)this.companion.getMaxHealth()), 8, 90, 4210752);
-            this.font.draw(pPoseStack, Component.literal("Inventory: " + this.companion.getInventory().getContainerSize()), 8, 100, 4210752);
-            
-            // Status display
-            String status = "Status: ";
-            if (this.companion.isMining()) status += "Mining ";
-            if (this.companion.isGathering()) status += "Gathering ";
-            if (this.companion.isFollowing()) status += "Following ";
-            if (status.equals("Status: ")) status += "Idle";
-            
-            this.font.draw(pPoseStack, Component.literal(status), 8, 110, 4210752);
+        if (this.menu.getCompanion() != null) {
+            var companion = this.menu.getCompanion();
+            this.font.draw(poseStack,
+                    Component.literal("Health: " + (int) companion.getHealth() + "/" + (int) companion.getMaxHealth()),
+                    188, 28, 0xD0D0D0);
+            this.font.draw(poseStack,
+                    Component.literal("Mode: " + companion.getModeName()),
+                    188, 38, 0xD0D0D0);
         }
-
-        pPoseStack.popPose();
     }
 
     @Override
