fc73695de9c9f65f73d4f5496c65892944a445bb
A1rdent <100067458+A1rdent@users.noreply.github.com>
2026-09-09 20:28:14 +0300

    Fix remaining 1.19.1 Fabric compilation issues

--- CompanionMod/src/main/java/com/example/companionmod/fabric/FabricClientMod.java
+++ CompanionMod/src/main/java/com/example/companionmod/fabric/FabricClientMod.java
@@ -1,26 +1,21 @@
 package com.example.companionmod.fabric;
 
+import com.example.companionmod.client.gui.CompanionScreen;
+import com.example.companionmod.client.render.CompanionEntityRenderer;
+import com.example.companionmod.registry.EntityTypeRegistry;
+import com.example.companionmod.registry.ScreenHandlerRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
-import com.example.companionmod.registry.ScreenHandlerRegistry;
-import com.example.companionmod.registry.EntityTypeRegistry;
-import com.example.companionmod.client.gui.CompanionScreen;
-import com.example.companionmod.client.render.CompanionEntityRenderer;
-
+@SuppressWarnings({"rawtypes","unchecked"})
 public class FabricClientMod implements ClientModInitializer {
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    @Override
-    public void onInitializeClient() {
-        LOGGER.info("CompanionMod (Fabric) client initializing");
-        // Register screen
+    private static final Logger LOGGER=LogManager.getLogger();
+    @Override public void onInitializeClient(){
+        LOGGER.info("CompanionMod client initializing");
         ScreenRegistry.register(ScreenHandlerRegistry.COMPANION, (handler, inventory, title) -> new CompanionScreen(handler, inventory, title));
-
-        // Register entity renderer
         EntityRendererRegistry.register(EntityTypeRegistry.COMPANION, CompanionEntityRenderer::new);
     }
 }
