b1407518a5d55f2eaf798bb36620a4ba3b72e1c6
A1rdent <100067458+A1rdent@users.noreply.github.com>
2026-09-09 20:28:11 +0300

    Fix remaining 1.19.1 Fabric compilation issues

--- CompanionMod/src/main/java/com/example/companionmod/fabric/FabricCompanionMod.java
+++ CompanionMod/src/main/java/com/example/companionmod/fabric/FabricCompanionMod.java
@@ -1,27 +1,24 @@
 package com.example.companionmod.fabric;
 
+import com.example.companionmod.common.entity.CompanionEntity;
+import com.example.companionmod.registry.CommandRegistry;
+import com.example.companionmod.registry.EntityTypeRegistry;
+import com.example.companionmod.registry.ItemRegistry;
+import com.example.companionmod.registry.ScreenHandlerRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.example.companionmod.registry.CommandRegistry;
-import com.example.companionmod.registry.EntityTypeRegistry;
-import com.example.companionmod.registry.ItemRegistry;
-import com.example.companionmod.registry.ScreenHandlerRegistry;
-import com.example.companionmod.common.entity.CompanionEntity;
-
 public class FabricCompanionMod implements ModInitializer {
-    public static final String MOD_ID = "companionmod";
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    @Override
-    public void onInitialize() {
-        LOGGER.info("CompanionMod (Fabric) initializing");
+    public static final String MOD_ID="companionmod";
+    private static final Logger LOGGER=LogManager.getLogger();
+    @Override public void onInitialize(){
+        LOGGER.info("CompanionMod initializing");
         ItemRegistry.init();
         EntityTypeRegistry.init();
         ScreenHandlerRegistry.init();
-        FabricDefaultAttributeRegistry.register(EntityTypeRegistry.COMPANION, CompanionEntity.createAttributes().build());
+        FabricDefaultAttributeRegistry.register(EntityTypeRegistry.COMPANION, CompanionEntity.createAttributes());
         CommandRegistry.init();
     }
 }
